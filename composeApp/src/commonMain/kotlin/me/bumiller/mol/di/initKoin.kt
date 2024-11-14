67d9e2ee30b04fad0a08e5e86edcea4dd21c1f62
Simon Bumiller <simon@bumiller.me>
2024-11-14 19:40:00 +0100

    5: Set up koin initialization

---
+++ composeApp/src/commonMain/kotlin/me/bumiller/mol/di/initKoin.kt
@@ -0,0 +1,18 @@
+package me.bumiller.mol.di
+
+import me.bumiller.mol.database.di.databaseModule
+import org.koin.core.context.startKoin
+import org.koin.dsl.KoinAppDeclaration
+
+/**
+ * Central entrypoint to start the koin service
+ *
+ * @param config Koin config that may differ from platform to platform
+ */
+fun initKoin(config: KoinAppDeclaration? = null) {
+    startKoin {
+        config?.invoke(this)
+
+        modules(databaseModule, viewmodelModule)
+    }
+}
