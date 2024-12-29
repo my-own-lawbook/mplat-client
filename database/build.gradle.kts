19137f162fe15b08e6d09dff63b886a1ab3fb997
Simon Bumiller <simon@bumiller.me>
2024-12-29 12:59:10 +0100

    7: COnfigure :database for testing

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -11,12 +11,11 @@ android {
     defaultConfig {
         compileSdk = 34
         minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 }
 
-room {
-    schemaDirectory("$projectDir/schemas")
-}
 
 kotlin {
     jvmToolchain(21)
@@ -36,12 +35,19 @@ kotlin {
             implementation(libs.room.runtime)
             implementation(libs.koin)
             implementation(libs.sqlite.driver)
+
+            implementation(libs.google.guava.workaround)
         }
 
         androidMain.dependencies {
             implementation(libs.koin.android)
         }
 
+        androidInstrumentedTest.dependencies {
+            implementation(libs.coroutines.test)
+            implementation(libs.androidx.test.junit)
+            implementation(libs.androidx.test.runner)
+        }
     }
 }
 
@@ -49,3 +55,15 @@ dependencies {
     add("ksp", libs.room.compiler)
 }
 
+
+room {
+    schemaDirectory("$projectDir/schemas")
+}
+
+// Workaround for property 'inputDirectory' cannot be changed
+// See https://slack-chats.kotlinlang.org/t/23153122/im-working-with-compose-multiplatform-project-for-android-an
+tasks.whenTaskAdded {
+    if (name.contains("copyRoomSchemasToAndroidTestAssetsDebugAndroidTest")) {
+        enabled = false
+    }
+}
