fb30b13cbd388e09ef5d415ba0fdf4d6a30c3fff
Simon Bumiller <simon@bumiller.me>
2024-12-31 17:05:31 +0100

    14: Remove broken xml sampel

--- composeApp/src/commonMain/kotlin/me/bumiller/mol/App.kt
+++ composeApp/src/commonMain/kotlin/me/bumiller/mol/App.kt
@@ -1,7 +1,5 @@
 package me.bumiller.mol
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
@@ -14,25 +12,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import mplat_client.composeapp.generated.resources.Res
-import mplat_client.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.imageResource
+import org.koin.compose.KoinContext
 
+/**
+ * Root composable that wraps the entirety of the app.
+ */
 @Composable
-fun App() {
+fun App() = KoinContext {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                Column(
-                    Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(imageResource(Res.drawable.compose_multiplatform), null)
-                }
             }
         }
     }
