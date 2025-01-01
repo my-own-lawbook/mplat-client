18cf2353a73f4c3719f16e5a32993b9b46f6649f
Simon Bumiller <simon@bumiller.me>
2025-01-01 13:31:07 +0100

    14: Added logic for loading initial top level location

--- composeApp/src/commonMain/kotlin/me/bumiller/mol/app/MolAppRoot.kt
+++ composeApp/src/commonMain/kotlin/me/bumiller/mol/app/MolAppRoot.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
 import me.bumiller.mol.ui.theme.MolTheme
 import org.koin.compose.KoinContext
 import org.koin.compose.viewmodel.koinViewModel
@@ -28,17 +29,22 @@ fun MolAppRoot(
     // Notify the parent when this screen is ready.
     // May be used for a loading screen or the like.
     LaunchedEffect(Unit) {
-        viewModel.stats.collectLatest {
-            if (it.isSuccess) {
+        viewModel.settings.combine(viewModel.topLevelLocation) { settings, location ->
+            settings.isSuccess && location.isSuccess
+        }.collectLatest {
+            if (it) {
                 onScreenReady()
             }
         }
     }
 
-    val settingsState by viewModel.stats.collectAsStateWithLifecycle()
-    val settings = settingsState.dataOrNull()
+    val settingsState by viewModel.settings.collectAsStateWithLifecycle()
+    val locationState by viewModel.topLevelLocation.collectAsStateWithLifecycle()
 
-    if (settings != null) {
+    val settings = settingsState.dataOrNull()
+    val location = locationState.dataOrNull()
+
+    if (settings != null && location != null) {
         MolTheme(
             windowSizeClass = windowSizeClass,
             colorMode = settings.colorMode,
--- composeApp/src/commonMain/kotlin/me/bumiller/mol/app/MolAppViewModel.kt
+++ composeApp/src/commonMain/kotlin/me/bumiller/mol/app/MolAppViewModel.kt
@@ -2,10 +2,14 @@ package me.bumiller.mol.app
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import me.bumiller.mol.data.UserSettingsSource
 import me.bumiller.mol.model.UserSettings
 import me.bumiller.mol.model.state.SimpleState
@@ -23,14 +27,36 @@ class MolAppViewModel(
 ) : ViewModel() {
 
     /**
+     * Will wait for the first settings emission and set the initial value for the top-level-location accordingly.
+     */
+    init {
+        viewModelScope.launch {
+            val settings = settings.first { it.isSuccess }.dataOrNull()!!
+
+            val initialLocation = if (settings.backendUrl == null) MolTopLevelLocation.Onboarding
+            else MolTopLevelLocation.Home
+
+            _topLevelLocation.emit(SimpleState.success(initialLocation))
+        }
+    }
+
+    /**
      * A state flow of the user settings
      */
-    val stats: StateFlow<SimpleState<UserSettings>> = settingsSource.settings
+    val settings: StateFlow<SimpleState<UserSettings>> = settingsSource.settings
         .map(SimpleState.Companion::success)
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5000),
             initialValue = SimpleState.Loading()
         )
+
+    private val _topLevelLocation =
+        MutableStateFlow<SimpleState<MolTopLevelLocation>>(SimpleState.loading())
+
+    /**
+     * A state flow containing the current top level location that should be displayed.
+     */
+    val topLevelLocation = _topLevelLocation.asStateFlow()
 
 }
