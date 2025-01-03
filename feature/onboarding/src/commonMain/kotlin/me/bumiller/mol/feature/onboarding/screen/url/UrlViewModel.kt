c0e25db61ab8185837a5a761e6edf8a57edec175
Simon Bumiller <simon@bumiller.me>
2025-01-03 10:52:38 +0100

    12: Added logic for checking connection to server in viewmodel

--- feature/onboarding/src/commonMain/kotlin/me/bumiller/mol/feature/onboarding/screen/url/UrlViewModel.kt
+++ feature/onboarding/src/commonMain/kotlin/me/bumiller/mol/feature/onboarding/screen/url/UrlViewModel.kt
@@ -1,14 +1,19 @@
 package me.bumiller.mol.feature.onboarding.screen.url
 
+import com.eygraber.uri.Url
+import kotlinx.coroutines.flow.first
+import me.bumiller.mol.common.ui.input.validation.ValidationError
 import me.bumiller.mol.common.ui.input.validation.validate
 import me.bumiller.mol.common.ui.viewmodel.MolViewModel
 import me.bumiller.mol.data.UserSettingsSource
+import me.bumiller.mol.network.ServerStatusChecker
 
 /**
  * Viewmodel for the screen allowing the user to adjust the url of the backend server.
  */
 class UrlViewModel(
-    private val settingsSource: UserSettingsSource
+    private val settingsSource: UserSettingsSource,
+    private val serverStatusChecker: ServerStatusChecker
 ) : MolViewModel<UrlUiEvent, UrlEvent>() {
 
     init {
@@ -33,12 +38,37 @@ class UrlViewModel(
         )
     }
 
-    private fun UrlUiEvent.Confirm.handle() {
+    private suspend fun UrlUiEvent.Confirm.handle() {
         updateUiState<UrlState> {
             it.copy(
                 url = it.url.validate()
             )
         }
+
+        if (formState.value.url.isError()) return
+
+        val urlWithScheme = "https://${formState.value.url.value}"
+
+        val canReachServer = withFetchState {
+            serverStatusChecker.checkServerConnection(urlWithScheme)
+        }
+
+        if (!canReachServer) {
+            updateUiState<UrlState> {
+                it.copy(
+                    url = it.url.copy(error = ValidationError.CantReachUrl)
+                )
+            }
+
+            return
+        }
+
+        val url = Url.parse(urlWithScheme)
+
+        val settings = settingsSource.settings.first()
+        settingsSource.update(settings.copy(backendUrl = url))
+
+        fireEvent(UrlEvent.Continue)
     }
 
 }
