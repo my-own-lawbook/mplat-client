7d331c214087a345a07d825dc695eb502275413b
Simon Bumiller <simon@bumiller.me>
2025-01-03 10:51:49 +0100

    12: Fixed ktor http-client initialization

--- network/src/commonMain/kotlin/me/bumiller/mol/network/impl/KtorServerStatusChecker.kt
+++ network/src/commonMain/kotlin/me/bumiller/mol/network/impl/KtorServerStatusChecker.kt
@@ -1,20 +1,29 @@
 package me.bumiller.mol.network.impl
 
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
+import kotlinx.io.IOException
 import me.bumiller.mol.network.ServerStatusChecker
+import java.nio.channels.UnresolvedAddressException
 
-private const val PingPath = "ping/"
+private const val PingPath = "api/v1/ping/"
 
 internal class KtorServerStatusChecker : ServerStatusChecker {
 
-    private val client: HttpClient = HttpClient()
+    private val client: HttpClient = HttpClient(CIO)
 
     override suspend fun checkServerConnection(url: String): Boolean {
-        val response = client.get("$url$PingPath")
+        val response = try {
+            client.get("$url$PingPath")
+        } catch (e: IOException) {
+            null
+        } catch (e: UnresolvedAddressException) {
+            null
+        }
 
-        return response.status == HttpStatusCode.OK
+        return response?.status == HttpStatusCode.OK
     }
 
 }
