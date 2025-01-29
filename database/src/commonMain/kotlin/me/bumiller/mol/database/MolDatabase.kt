abfa4333c8c42e6b34e396912f172bc91d195068
Simon Bumiller <simon@bumiller.me>
2025-01-29 21:44:45 +0100

    milestone-7: Added entities to database

--- database/src/commonMain/kotlin/me/bumiller/mol/database/MolDatabase.kt
+++ database/src/commonMain/kotlin/me/bumiller/mol/database/MolDatabase.kt
@@ -3,12 +3,17 @@ package me.bumiller.mol.database
 import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import me.bumiller.mol.database.entities.BookEntity
+import me.bumiller.mol.database.entities.EntryEntity
+import me.bumiller.mol.database.entities.ForeignUserEntity
+import me.bumiller.mol.database.entities.InvitationEntity
+import me.bumiller.mol.database.entities.SectionEntity
 
 /**
  * Class that represents the sql database
  */
 @Database(
-    entities = [],
+    entities = [BookEntity::class, EntryEntity::class, ForeignUserEntity::class, InvitationEntity::class, SectionEntity::class],
     version = MolDatabase.DB_VERSION
 )
 @ConstructedBy(MolDatabaseConstructor::class)
