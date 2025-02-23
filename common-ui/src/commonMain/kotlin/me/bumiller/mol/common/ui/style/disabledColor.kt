e121faae8d7b39792ebec456af3fb2cc06ac8280
Simon Bumiller <simon@bumiller.me>
2025-02-23 17:02:52 +0100

    44: Added helper function to create disabled color tones

--- common-ui/src/commonMain/kotlin/me/bumiller/mol/common/ui/style/disabledColor.kt
+++ common-ui/src/commonMain/kotlin/me/bumiller/mol/common/ui/style/disabledColor.kt
@@ -1,16 +1,20 @@
 package me.bumiller.mol.common.ui.style
 
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 
-private const val Factor = 0.39F
+private const val DisabledFactor = 0.39F
 
 /**
  * Creates the disabled variant by lowering the alpha value of the font styles color.
  *
  * @return The same font style with a lowered alpha value
  */
+@Composable
 fun TextStyle.disabledColor(): TextStyle = copy(
-    color = color.copy(
-        alpha = color.alpha * Factor
-    )
+    color = LocalContentColor.current.disabledColor()
 )
+
+private fun Color.disabledColor() = copy(alpha = alpha * DisabledFactor)
