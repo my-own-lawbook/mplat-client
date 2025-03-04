dbc561ef22d0fad3ffd9fef65886f2e8383fceea
Simon Bumiller <simon@bumiller.me>
2025-03-04 16:13:38 +0100

    50: Added dependency injection for the sync adapter

---
+++ sync/src/commonMain/kotlin/me/bumiller/mol/sync/di/syncModule.kt
@@ -0,0 +1,25 @@
+package me.bumiller.mol.sync.di
+
+import me.bumiller.mol.sync.SyncAdapter
+import me.bumiller.mol.sync.impl.NetworkDatabaseSyncAdapter
+import org.koin.dsl.module
+
+/**
+ * Koin module for the :sync module.
+ */
+val syncModule = module {
+    single<SyncAdapter> {
+        NetworkDatabaseSyncAdapter(
+            get(),
+            get(),
+            get(),
+            get(),
+            get(),
+            get(),
+            get(),
+            get(),
+            get(),
+            get()
+        )
+    }
+}
