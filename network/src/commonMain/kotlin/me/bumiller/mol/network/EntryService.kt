1a3330d46d71abee8d3df53bd5881b5b05c58549
Simon Bumiller <simon@bumiller.me>
2025-03-04 16:14:42 +0100

    50: Added abstraction for network services

--- network/src/commonMain/kotlin/me/bumiller/mol/network/EntryService.kt
+++ network/src/commonMain/kotlin/me/bumiller/mol/network/EntryService.kt
@@ -1,9 +1,9 @@
 package me.bumiller.mol.network
 
-import me.bumiller.mol.network.base.SimpleResourceService
+import me.bumiller.mol.network.base.ResourceParentService
 import me.bumiller.mol.network.response.LawEntryResponse
 
 /**
  * Service that accesses the law-entry resource on the rest api.
  */
-interface EntryService : SimpleResourceService<LawEntryResponse>
+interface EntryService : ResourceParentService<LawEntryResponse>
--- network/src/commonMain/kotlin/me/bumiller/mol/network/SectionService.kt
+++ network/src/commonMain/kotlin/me/bumiller/mol/network/SectionService.kt
@@ -1,9 +1,9 @@
 package me.bumiller.mol.network
 
-import me.bumiller.mol.network.base.SimpleResourceService
+import me.bumiller.mol.network.base.ResourceParentService
 import me.bumiller.mol.network.response.LawSectionResponse
 
 /**
  * Service that accesses the law-sections resource on the rest api.
  */
-interface SectionService : SimpleResourceService<LawSectionResponse>
+interface SectionService : ResourceParentService<LawSectionResponse>
