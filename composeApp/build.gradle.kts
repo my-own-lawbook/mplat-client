f09748b74b207bf3a2071c3556854c59cf0f0380
Simon Bumiller <simon@bumiller.me>
2025-03-05 23:07:22 +0100

    1: Add name, description, license and vendor information

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -55,8 +55,13 @@ kotlin {
 
 private val iconFileIco = project.file("./src/jvmMain/resources/icon.ico")
 private val iconFilePng = project.file("./src/jvmMain/resources/icon.png")
+private val licenseFile = project.file("./../LICENSE")
 
 private val appVersion = "0.0.4"
+private val appName = "Civoris"
+private val appDescription =
+    "Manage rules and conventions by creating a structured dictionary of guidelines! Collaborate with others and manage access, visibility and more."
+private val appVendor = "Simon Bumiller"
 
 compose.desktop.application {
     // Workaround according to https://github.com/JetBrains/compose-multiplatform/issues/3818#issuecomment-1795163561
@@ -71,6 +76,9 @@ compose.desktop.application {
         targetFormats(TargetFormat.Exe, TargetFormat.Msi, TargetFormat.Deb)
 
         packageVersion = appVersion
+        packageName = appName
+        description = appDescription
+        licenseFile = licenseFile
 
         linux {
             iconFile.set(iconFileIco)
@@ -79,6 +87,5 @@ compose.desktop.application {
         windows {
             iconFile.set(iconFilePng)
         }
-        packageName = "me.bumiller.mol"
     }
 }
