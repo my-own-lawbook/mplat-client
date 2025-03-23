ebc33246ada21db67e53d26b1546ad656a6cd4b7
Simon Bumiller <simon@bumiller.me>
2025-03-23 22:38:18 +0100

    62: Added otp component to email screen

--- feature/auth/src/commonMain/kotlin/me/bumiller/mol/feature/auth/screen/email/EmailScreen.kt
+++ feature/auth/src/commonMain/kotlin/me/bumiller/mol/feature/auth/screen/email/EmailScreen.kt
@@ -3,27 +3,23 @@ package me.bumiller.mol.feature.auth.screen.email
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.datetime.Clock
 import me.bumiller.mol.auth.Res
 import me.bumiller.mol.auth.cd_email_screen
 import me.bumiller.mol.auth.email_screen
 import me.bumiller.mol.auth.email_screen_description
-import me.bumiller.mol.auth.email_screen_input_token_label
 import me.bumiller.mol.auth.email_screen_resend_button_label
 import me.bumiller.mol.auth.email_screen_title
 import me.bumiller.mol.auth.email_screen_verify_button_label
 import me.bumiller.mol.common.ui.viewmodel.ViewModelScope
-import me.bumiller.mol.ui.components.MolTextField
+import me.bumiller.mol.feature.auth.screen.component.OtpTextViews
 import me.bumiller.mol.ui.components.WideButton
 import me.bumiller.mol.ui.components.WideOutlinedButton
 import me.bumiller.mol.ui.layout.AppBarLayoutWithImage
@@ -72,20 +68,29 @@ private fun EmailScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+            verticalArrangement = Arrangement.SpaceAround,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            MolTextField(
-                value = formState.token,
-                onValueChange = { onEvent(EmailUiEvent.ChangeToken(it)) },
-                label = {
-                    Text(stringResource(Res.string.email_screen_input_token_label))
-                },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = ImeAction.Done
-                ),
-                keyboardActions = KeyboardActions { onEvent(EmailUiEvent.Continue) }
+            OtpTextViews(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                isError = formState.token.isError(),
+                onFinished = {
+                    onEvent(EmailUiEvent.ChangeToken(it))
+                    onEvent(EmailUiEvent.Continue)
+                }
             )
+            //MolTextField(
+            //    value = formState.token,
+            //    onValueChange = { onEvent(EmailUiEvent.ChangeToken(it)) },
+            //    label = {
+            //        Text(stringResource(Res.string.email_screen_input_token_label))
+            //    },
+            //    keyboardOptions = KeyboardOptions.Default.copy(
+            //        imeAction = ImeAction.Done
+            //    ),
+            //    keyboardActions = KeyboardActions { onEvent(EmailUiEvent.Continue) }
+            //)
 
             WideButton(
                 onClick = { onEvent(EmailUiEvent.Continue) }
